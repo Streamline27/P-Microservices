c3a98f468fbcf0295cb244c17bd2f59d258b96b6
Vladislav Yakushin <vladislavs.jakusins@citadele.lv>
2019-01-31 22:45:31 +0200

    Support for immutable lombok classes

--- Loans/src/test/java/lv/citadele/loans/LoansApplicationTests.java
+++ Loans/src/test/java/lv/citadele/loans/LoansApplicationTests.java
@@ -1,17 +1,53 @@
 package lv.citadele.loans;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import javax.persistence.Entity;
+import java.io.IOException;
+
+@RunWith(MockitoJUnitRunner.class)
 public class LoansApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void x() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Login user = Login.builder()
+                .name("VladJa")
+                .surname("Jakushin")
+                .build();
+
+        String s = mapper.writeValueAsString(user);
+        System.out.println(s);
+
+        Login user1 = mapper.readValue(s, Login.class);
+
+        System.out.println(user1);
     }
 
+    @Data
+    @Builder
+    @Setter(AccessLevel.NONE)
+    static class User {
+        private final String name;
+        private final String surname;
+    }
+
+    /**
+     * lombok.config -> lombok.anyConstructor.addConstructorProperties = true
+     */
+    @Entity
+    @Builder
+    static class Login {
+        private final String name;
+        private final String surname;
+    }
 }
 
